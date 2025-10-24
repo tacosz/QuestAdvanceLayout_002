ac145652f6c1c8c58f8b4be7a60c5aaf0e7f6fff
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-24 15:02:34 +0700

    spacer antar layout

--- app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
@@ -1,7 +1,9 @@
 package com.example.pertemuan4pam
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,5 +32,7 @@ fun ActivitasPertama(modifier: Modifier){
             text = stringResource(id = R.string.univ),
             fontSize = 18.sp
         )
+
+        Spacer(modifier = Modifier.height(height = 25.dp))
     }
 }
