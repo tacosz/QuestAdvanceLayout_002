55ecdb3504386e3ec60223204ebdc112b9dbf93c
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-24 15:00:40 +0700

    layout Text pertama prodi

--- app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
@@ -3,10 +3,14 @@ package com.example.pertemuan4pam
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import java.lang.reflect.Modifier
 
 @Composable
@@ -16,6 +20,10 @@ fun ActivitasPertama(modifier: Modifier){
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-
+        Text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
