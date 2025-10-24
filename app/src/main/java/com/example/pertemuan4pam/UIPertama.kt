4a3fb2cfac81cdf08f81bba73ff37e46be5a0d77
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-24 14:56:05 +0700

    padding pada column

--- app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
@@ -1,9 +1,16 @@
 package com.example.pertemuan4pam
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
+    Column (modifier = Modifier
+        .padding(top = 100.dp)){
 
+    }
 }
