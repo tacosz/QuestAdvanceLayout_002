6e1c9e6620568b0452b2f82b0a1d79443128e4d7
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-24 19:06:15 +0700

    background utama layout

--- app/src/main/java/com/example/pertemuan4pam/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan4pam/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan4PAMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ActivitasPertama(
+                    BMWProfile(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/example/pertemuan4pam/TugasP4.kt
+++ app/src/main/java/com/example/pertemuan4pam/TugasP4.kt
@@ -1,2 +1,24 @@
 package com.example.pertemuan4pam
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BMWProfile(modifier: Modifier) {
+    val backgroundColor = Color(0xFF121212)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {  }
+}
