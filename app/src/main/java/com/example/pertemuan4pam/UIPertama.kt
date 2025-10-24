c9fbcd5c1b38359e8c7322bcbb3f55257303fccf
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-24 14:57:01 +0700

    fill max size

--- app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.pertemuan4pam
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,7 +11,8 @@ import java.lang.reflect.Modifier
 @Composable
 fun ActivitasPertama(modifier: Modifier){
     Column (modifier = Modifier
-        .padding(top = 100.dp)){
+        .padding(top = 100.dp)
+        .fillMaxSize()){
 
     }
 }
