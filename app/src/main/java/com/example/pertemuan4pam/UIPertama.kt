91a1e9c3b5d9e8a1c0665823c5dc7d3d5f9d297d
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-24 14:58:31 +0700

    tata letak

--- app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4pam/UIPertama.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
@@ -12,7 +13,9 @@ import java.lang.reflect.Modifier
 fun ActivitasPertama(modifier: Modifier){
     Column (modifier = Modifier
         .padding(top = 100.dp)
-        .fillMaxSize()){
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
 
     }
 }
