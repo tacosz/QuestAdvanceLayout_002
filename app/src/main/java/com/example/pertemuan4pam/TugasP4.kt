850c546e830b23d56d77394b546ec1f4d5138785
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-25 12:44:04 +0700

    menambahkan icon sosial media

--- app/src/main/java/com/example/pertemuan4pam/TugasP4.kt
+++ app/src/main/java/com/example/pertemuan4pam/TugasP4.kt
@@ -3,6 +3,7 @@ package com.example.pertemuan4pam
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -10,6 +11,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -34,16 +37,36 @@ fun BMWProfile(modifier: Modifier) {
             .padding(70.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.bmw),
-            contentDescription = "Profile Picture",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(150.dp)
-                .clip(CircleShape)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+        Card(
+            modifier = Modifier.size(140.dp),
+            shape = CircleShape,
+            elevation = CardDefaults.cardColors(containerColor = Color.Transparent)
+        ) {
+            val bmw = painterResource(R.drawable.bmw)
+            Image(
+                painter = bmw,
+                contentDescription = "bmw",
+                modifier = Modifier
+                    .size(140.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+        }
 
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(10.dp)
+
+        ) {
+            val x = painterResource(id = R.drawable.x)
+            Image(
+                painter = x,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(size = 100.dp)
+            )
+        }
         Text(
             text = stringResource(id = R.string.name),
             fontSize = 18.sp,
